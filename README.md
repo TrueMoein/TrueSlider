2aaf1f000abe0f5215626d7d3eae10bc63ebb58d
Moein Alizadeh <truemoein@gmail.com>
2019-06-16 13:25:23 +0430

    Update README.

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # TrueSlider
+
+How to use:
+
+```html
+<!doctype html>
+<html lang="en">
+<head>
+ <title>TrueSlider</title>
+ <link rel="stylesheet" href="https://unpkg.com/trueslider/dist/trueslider.css">
+</head>
+<body>
+  <div id="slider"></div>
+  
+  <script src="https://unpkg.com/trueslider/dist/trueslider.js"></script>
+</body>
+</html>
+```
+
+```javascript
+window.TrueSlider({
+  el: '#slider',
+  width: 600,
+  height: 350,
+  images: [
+    `https://picsum.photos/id/100/600/350`,
+    `https://picsum.photos/id/101/600/350`,
+    `https://picsum.photos/id/102/600/350`,
+  ]
+});
+
+```
+
+you can install it with **npm**:
+
+``npm install trueslider``
+
+or with **yarn**:
+
+``yarn add trueslider``
+
+### options:
+
+
--- src/trueSliderExample.js
+++ src/trueSliderExample.js
@@ -3,10 +3,18 @@ window.TrueSlider({
   width: 600,
   height: 350,
   images: [
-    'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
-    'https://cdn.pixabay.com/photo/2018/10/30/16/06/water-lily-3784022__340.jpg',
-    'https://data.whicdn.com/images/329858934/original.jpg?t=1556728606',
-    'https://www.opencollege.info/wp-content/uploads/2016/02/relaxation-skills.jpg',
-    'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    `https://picsum.photos/id/100/600/350`,
+    `https://picsum.photos/id/101/600/350`,
+    `https://picsum.photos/id/102/600/350`,
+    `https://picsum.photos/id/103/600/350`,
+    `https://picsum.photos/id/104/600/350`,
+    `https://picsum.photos/id/120/600/350`,
+    `https://picsum.photos/id/106/600/350`,
+    `https://picsum.photos/id/107/600/350`,
+    `https://picsum.photos/id/108/600/350`,
+    `https://picsum.photos/id/109/600/350`,
+    `https://picsum.photos/id/110/600/350`,
+    `https://picsum.photos/id/111/600/350`,
+    `https://picsum.photos/id/112/600/350`
   ]
 });
